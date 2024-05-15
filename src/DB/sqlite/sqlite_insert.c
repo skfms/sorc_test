eee5795c4edb4d9790fa6d6cf61b12c2609f6139
skfms <leegs11@gmail.com>
2024-05-16 08:50:46 +0900

    sqlite source add

---
+++ src/DB/sqlite/sqlite_insert.c
@@ -0,0 +1,49 @@
+// gcc -o sqlite_insert sqlite_insert.c -l sqlite3
+
+#include <sqlite3.h>
+#include <stdio.h>
+
+int main(void)
+{
+    sqlite3 *db;
+    char *err_msg = 0;
+    
+    int rc = sqlite3_open(":memory:", &db);
+    
+    if (rc != SQLITE_OK)
+    {
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        
+        return 1;
+    }
+    
+    char *sql = "CREATE TABLE Friends(Id INTEGER PRIMARY KEY, Name TEXT);"
+    "INSERT INTO Friends(Name) VALUES ('Tom');"
+    "INSERT INTO Friends(Name) VALUES ('Rebecca');"
+    "INSERT INTO Friends(Name) VALUES ('Jim');"
+    "INSERT INTO Friends(Name) VALUES ('Roger');"
+    "INSERT INTO Friends(Name) VALUES ('Robert');";
+        
+    
+    rc = sqlite3_exec(db, sql, 0, 0, &err_msg);
+    
+    if (rc != SQLITE_OK )
+    {
+        fprintf(stderr, "Failed to create table\n");
+        fprintf(stderr, "SQL error: %s\n", err_msg);
+        sqlite3_free(err_msg);
+    }
+    else
+    {
+        fprintf(stdout, "Table Friends created successfully\n");
+    }
+    
+    int last_id = sqlite3_last_insert_rowid(db);
+    printf("The last Id of the inserted row is %d\n", last_id);
+
+    sqlite3_close(db);
+    
+    return 0;
+}
+
